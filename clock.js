5860f7321d147f11acd29e705f97554f4964202a
unknown <juliusgba@outlook.com>
2022-06-27 08:23:39 +0100

    Added clock.js

---
+++ clock.js
@@ -0,0 +1,41 @@
+//create a canvas object fro HTML element
+var canvas = document.getElementById('canvas');
+//create a 2d drawing object
+var ctx = canvas.getContext('2d');
+//calculate the clock radius by using the height
+var radius = canvas.height / 2;
+//remap the x and y axis to the center of the canvas
+ctx.translate(radius, radius);
+//reduce clock radius by 90%
+radius = radius * 0.90;
+
+setInterval(drawClock, 1000); //run the drawClock function every second.
+
+function drawClock(){
+    drawFace(ctx, radius);
+    drawNumbers(ctx, radius);
+    drawTime(ctx, radius);
+}
+
+function drawFace(ctx, radius){
+    var grad;
+    //draw white circle for the face
+    ctx.beginPath();
+    ctx.arc(0,0,radius,0,2*Math.PI);
+    ctx.fillStyle = "White";
+    ctx.fill();
+    // create a radial gradient (inner, middle, and outer edge of clock)
+    grad = ctx.createRadialGradient(0,0,radius*0.95, 0,0,radius*1.05);
+    grad.addColorStop(0, '#333');
+    grad.addColorStop(0.5, 'white');
+    grad.addColorStop(1, '#333');
+    //define gradient as stroke style
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius*0.1;
+    ctx.stroke(); 
+    //draw the center of the clock
+    ctx.beginPath();
+    ctx.arc(0,0, radius*0.1,0,2*Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
+}
