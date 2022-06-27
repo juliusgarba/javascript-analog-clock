db95cdaff4e07b5fe8954c73b810c581b9338c91
unknown <juliusgba@outlook.com>
2022-06-27 11:45:04 +0100

    update

--- clock.js
+++ clock.js
@@ -39,3 +39,55 @@ function drawFace(ctx, radius){
     ctx.fillStyle = '#333';
     ctx.fill();
 }
+
+function drawNumbers(ctx, radius) {
+    var ang;
+    var num;
+    ctx.font = radius*0.15 + "px arial"; //set font at 15% of radius
+    ctx.textBaseline = "middle"; //set text alignment to middle
+    ctx.textAlign = "center"; //set text alignment to center
+    for(num=1; num < 13; num++){ //calculate the print position for each number
+        ang = num *Math.PI /6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius*0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius*0.85);
+        ctx.rotate(-ang);
+    }
+}
+
+function drawTime(ctx, radius){
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    //hour
+    hour = hour%12;
+    //calculate angle of hour hand
+    hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
+    //make hour hand 50% of canvas's radius
+    drawHand(ctx, hour, radius*0.5, radius*0.07);
+    //minute
+    //calculate angle of minute hand
+    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+    //make minute hand 80% of canvas's radius
+    drawHand(ctx, minute, radius*0.8, radius*0.07);
+    //second
+    //calculate angle of second hand
+    second=(second*Math.PI/30);
+    //make second hand 90% of canvas's radius
+    drawHand(ctx, second, radius*0.9, radius*0.02);
+}
+
+function drawHand(ctx, pos, length, width){
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0,0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
+}
